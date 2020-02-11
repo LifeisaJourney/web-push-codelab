177f314d8db33928cf647c6244add43502caa52c
Howie Zhu <hwie.zhu@gmail.com>
2020-02-11 15:11:16 -0500

    updating subscription to server, rendering display

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -117,3 +117,18 @@ function updateBtn() {
   pushButton.disabled = false;
 }
 
+function updateSubscriptionOnServer(subscription) {
+  // TODO: Send subscription to application server
+
+  const subscriptionJson = document.querySelector('.js-subscription-json');
+  const subscriptionDetails =
+    document.querySelector('.js-subscription-details');
+
+  if (subscription) {
+    subscriptionJson.textContent = JSON.stringify(subscription);
+    subscriptionDetails.classList.remove('is-invisible');
+  } else {
+    subscriptionDetails.classList.add('is-invisible');
+  }
+}
+
