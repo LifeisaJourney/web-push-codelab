3c080a0380a7d5d09b02804b8e440b5b2d52cb52
Howie Zhu <hwie.zhu@gmail.com>
2020-02-11 13:41:41 -0500

    service worker and push is working on chrome dev tools with web server

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -21,12 +21,30 @@
 
 'use strict';
 
-const applicationServerPublicKey = '<Your Public Key>';
+const applicationServerPublicKey = 'BFNNOrJIGX-HOzMpoF3Bwqd3U6mu7aZ8Yr1enfA5H0xRh3oEcgl4aHD8CeMHFaNv95a-7lGMKEQwMSHpyNQaVsE';
 
 const pushButton = document.querySelector('.js-push-btn');
 
 let isSubscribed = false;
 let swRegistration = null;
+
+if ('serviceWorker' in navigator && 'PushManager' in window) {
+  console.log('Service Worker and Push are supported');
+
+  navigator.serviceWorker.register('sw.js')
+  .then(function(swReg) {
+    console.log('Service Worker is registered', swReg);
+
+    swRegistration = swReg;
+  })
+  .catch(function(error) {
+    console.error('Service Worker Error', error);
+  });
+} else {
+  console.warn('Push messaging is not supported');
+  pushButton.textContent = 'Push Not Supported';
+}
+
 
 function urlB64ToUint8Array(base64String) {
   const padding = '='.repeat((4 - base64String.length % 4) % 4);
